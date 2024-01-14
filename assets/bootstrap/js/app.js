f988a9214a95d8852d0f54ee41cb0053c5ae9136
Matt Hickling <m.hickling@hotmail.co.uk>
2024-01-14 17:56:50 +0000

    fonts added and bits fininshed

--- assets/bootstrap/js/app.js
+++ assets/bootstrap/js/app.js
@@ -25,3 +25,54 @@ var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
 yearElement.textContent = currentYear;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Check if the user has previously accepted cookies
+    if (!getCookie("cookieConsent")) {
+        // If not, show the cookie consent message
+        document.getElementById("cookie-consent").style.display = "block";
+    }
+});
+
+function acceptCookies() {
+    // Set a cookie to remember that the user has accepted cookies
+    setCookie("cookieConsent", "accepted", 365);
+    // Hide the cookie consent message
+    document.getElementById("cookie-consent").style.display = "none";
+}
+
+function rejectCookies() {
+    // Delete the cookie to reject cookies
+    deleteCookie("cookieConsent");
+    // Hide the cookie consent message
+    document.getElementById("cookie-consent").style.display = "none";
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+function deleteCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
--- assets/contact.php
+++ assets/contact.php
@@ -2,6 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $phone = $_POST["phone"];
     $message = $_POST["message"];
 
     $to = "your-email@example.com";
@@ -12,6 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n";
+    $email_content .= "Phone: $phone\n";
     $email_content .= "Message:\n$message";
 
     mail($to, $subject, $email_content, $headers);
